dcb56e5cae1b19acc3cfb3a4257cd52539fff347
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-03-10 21:33:12 -0700

    Header.js

---
+++ src/components/layout/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FaPizzaSlice } from 'react-icons/fa';
+
+
+
+export const Header = () => {
+    const x = 1;
+
+
+    return <header className="header" data-testid="header">
+        <nav>
+            <div className="logo">
+                <img src="/image/logo.png" alt="Todoist" />
+            </div>
+            <div className="settings">
+                <ul>
+                    <li>+</li>
+                    <li>
+                        <FaPizzaSlice />
+                    </li>
+                    <li>+</li>
+                </ul>
+            </div>
+        </nav>
+    </header>;
+}
