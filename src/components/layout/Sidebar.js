0e9073cf533a57fec24797e3b5dc2c8e8f3b3ae2
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-03-12 16:41:48 -0700

    sidebar

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Sidebar = () =>
+    <div className="sidebar" data-test-id="sidebar">
+        <ul className="sidebar_generic">
+            <li>Inbox</li>
+            <li>Today</li>
+            <li>Next 7 days</li>
+        </ul>
+    </div>
