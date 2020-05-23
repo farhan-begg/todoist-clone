1c4ae223c2ebcb62d5997334927f6468bdef8f5c
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-05-22 23:01:20 -0700

    connected firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 
 // Data Base 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: "AIzaSyDX53yT0mmbUSo1Ay0m_9znAITgpf2gBvk",
+    authDomain: "todoist-clone-farhan.firebaseapp.com",
+    databaseURL: "https://todoist-clone-farhan.firebaseio.com",
+    projectId: "todoist-clone-farhan",
+    storageBucket: "todoist-clone-farhan.appspot.com",
+    messagingSenderId: "362096276724",
+    appId: "1:362096276724:web:9519137854768f629b9aa1"
 });
 
 export { firebaseConfig as firebase };
